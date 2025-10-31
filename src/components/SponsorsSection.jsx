64bc488cdeea6c59a0c9ec92316763a651fd24ad
prashilthul <prashil1411@gmail.com>
2025-10-31 19:29:36 +0530

    minor changes
    Signed-off-by: prashilthul <prashil1411@gmail.com>

--- src/components/SponsorsSection.jsx
+++ src/components/SponsorsSection.jsx
@@ -1,29 +1,41 @@
-'use client'
+"use client";
 
 import { Carousel_003 } from "@/components/ui/skiper-ui/skiper49";
 
 // Using the carousel component with custom images
 export default function SponsorsSection() {
-  const images = [
-    {
-      src: "/images/Desktop_1.jpg",
-      alt: "Description 1",
-    },
-    {
-      src: "/images/Desktop_1.jpg",
-      alt: "Description 2",
-    },
-    // ... more images
-  ];
+	const images = [
+		{
+			src: "/images/Desktop_1.jpg",
+			alt: "Description 1",
+		},
+		{
+			src: "/images/Desktop_1.jpg",
+			alt: "Description 2",
+		},
+		{
+			src: "/images/Desktop_1.jpg",
+			alt: "Description 1",
+		},
+		{
+			src: "/images/Desktop_1.jpg",
+			alt: "Description 2",
+		},
 
-  return (
-    <Carousel_003
-      images={images}
-      showPagination={true}
-      showNavigation={true}
-      loop={true}
-      autoplay={true}
-      spaceBetween={0}
-    />
-  );
-};
+		// ... more images
+	];
+
+	return (
+		<div className="justify-center flex items-center w-full h-full">
+			<Carousel_003
+				className={`flex justify-center w-full h-full items-center`}
+				images={images}
+				showPagination={true}
+				showNavigation={true}
+				loop={true}
+				autoplay={true}
+				spaceBetween={0}
+			/>
+		</div>
+	);
+}
